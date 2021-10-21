59736e6c2400ba05696b99cd0cf713b006766c77
Francisco Purdom <fpurdom@f1r5s19.codam.nl>
2021-10-21 15:35:51 +0200

    three

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -8,6 +8,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst)
 		return (NULL);
 	new = ft_lstnew(f(lst->content));
+	if (!new)
+	{
+		ft_lstclear(&ret, del);
+		return (NULL);
+	}
 	ret = new;
 	while (lst)
 	{
